8159b28783e0ea8fd7901d213d903a09b8faca22
今西レオ <imanishi8033@gmail.com>
2021-04-24 11:58:23 +0900

    Firebaseを導入しました

--- SlackTableView-Sample/AppDelegate.swift
+++ SlackTableView-Sample/AppDelegate.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         Router.showRoot(window: window)
